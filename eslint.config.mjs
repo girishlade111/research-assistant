d096c30a3ed68c1cb555c603ad31d90b9e9c3613
Girish Lade <girishlade111@gmail.com>
2026-04-29 03:41:21 -0700

    refactor: remove unused typescript-eslint dependency and simplify eslint configuration ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,16 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-import tseslint from "typescript-eslint";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  ...tseslint.configs.recommended,
   {
     rules: {
       "react-hooks/purity": "off",
     },
   },
-  // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
